c4fbbcb1e6293995f01b3c98a74065a340f529a1
Suzit Libi <sujit_libi@hotmail.com>
2018-08-30 17:41:52 +0545

    Finishing Redux implementation

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -3,6 +3,20 @@ import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from './types';
 export const getItems = () => {
     return {
         type: GET_ITEMS,
-        
+
     }
-}
+};
+
+export const deleteItem = (id) => {
+    return {
+        type: DELETE_ITEMS,
+        payload: id
+    }
+};
+
+export const addItem = (item) => {
+    return {
+        type: ADD_ITEMS,
+        payload: item
+    }
+};
